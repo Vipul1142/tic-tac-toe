61ad5c86ce5dbcef8d67c1e4aa7c399b35f534cb
Vipul Jha <vipuljha1142@gmail.com>
2020-04-18 21:12:30 +0530

    menu changed

--- draw.js
+++ draw.js
@@ -23,9 +23,9 @@ function showTitle() {
 function welcomeScreen(params) {
     showTitle();
     welcome = "   WELCOME";
-    optionMsg = "1.Play\t\t2.Stats\t\t3.Style\t\t0.Exit";
+    optionMsg = "1. Play Game\t\t2. Show Stats\t\t3. Change Style\t\t0. Exit";
     log(repeater(newt, 5) + welcome + newl);
-    log("\n\t\t  " + optionMsg);
+    log("\n\t" + optionMsg);
     log();
 }
 module.exports = {
