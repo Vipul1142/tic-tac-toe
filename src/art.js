52e40c1452d57ce4e703c8c936d15dc35393ac8f
Vipul Jha <vipuljha1142@gmail.com>
2020-04-18 21:14:45 +0530

    changed filenames

---
+++ src/art.js
@@ -0,0 +1,34 @@
+const {
+    repeater,
+    log,
+    clear
+} = require("./nucleas")
+
+const color = require("colors/safe");
+
+const newl = "\n";
+const newt = "\t";
+
+function showTitle() {
+    clear();
+    log("\n\n")
+    log("\t\t" + "____       ____    ____   ____    ____    ____   ____     ____");
+    log("\t\t" + " ||   ||  ||  ||    ||   ||  ||  ||  ||    ||   ||  ||  ||    ");
+    log("\t\t" + " ||   ||  ||        ||   ||==||  ||        ||   ||  ||  ||----");
+    log("\t\t" + " ||   ||  ||__||    ||   ||  ||  ||__||    ||   ||__||  ||____");
+    log(repeater(newt, 9) + "By Vipul");
+    log();
+}
+
+function welcomeScreen(params) {
+    showTitle();
+    welcome = "   WELCOME";
+    optionMsg = "1. Play Game\t\t2. Show Stats\t\t3. Change Style\t\t0. Exit";
+    log(repeater(newt, 5) + welcome + newl);
+    log("\n\t" + optionMsg);
+    log();
+}
+module.exports = {
+    showTitle,
+    welcomeScreen
+}
