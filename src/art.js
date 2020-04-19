7c8b08c6f0e8f3dd4f472708206ddde893c38f1d
Vipul Jha <vipuljha1142@gmail.com>
2020-04-20 00:21:34 +0530

    made good changes

--- src/art.js
+++ src/art.js
@@ -5,27 +5,23 @@ const {
     repeater
 } = require("./nucleas")
 
-const newl = "\n";
-const newt = "\t";
-
 function showTitle() {
     clear();
     log("\n\n")
-    log("\t\t" + "____       ____    ____   ____    ____    ____   ____     ____");
-    log("\t\t" + " ||   ||  ||  ||    ||   ||  ||  ||  ||    ||   ||  ||  ||    ");
-    log("\t\t" + " ||   ||  ||        ||   ||==||  ||        ||   ||  ||  ||----");
-    log("\t\t" + " ||   ||  ||__||    ||   ||  ||  ||__||    ||   ||__||  ||____");
-    log(repeater(newt, 9) + "By Vipul");
-    log(newl + newl);
+    log("\t\t" + "____     ____    ____  ____   ____    ____  ____    ____");
+    log("\t\t" + " ||  || ||  ||    ||  ||  || ||  ||    ||  ||  || ||    ");
+    log("\t\t" + " ||  || ||    --- ||  ||==|| ||    --- ||  ||  || ||----");
+    log("\t\t" + " ||  || ||__||    ||  ||  || ||__||    ||  ||__|| ||____");
+    log(repeater("\t", 8) + "By Vipul");
+    log("\n\n");
 }
 
 function welcomeScreen(params) {
     showTitle();
-    welcome = "   WELCOME";
-    optionMsg = "1. Play Game\t\t2. Show Stats\t\t3. Change Style\t\t0. Exit";
-    log(repeater(newt, 5) + welcome + newl);
-    log("\n\t" + optionMsg);
-    log();
+    welcome = "WELCOME\n\n";
+    optionMsg = "\t1. Play Game\t\t2. Show Stats\t\t3. Change Style\t\t0. Exit\n";
+    log(repeater("\t", 5) + welcome);
+    log(optionMsg);
 }
 module.exports = {
     showTitle,
