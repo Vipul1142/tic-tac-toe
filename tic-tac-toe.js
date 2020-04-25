d2bd55b03f8fbde0c25727257a505b1ca39a04cf
Vipul Jha <vipuljha1142@gmail.com>
2020-04-25 13:12:43 +0530

    added switch case to be in the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,29 @@
 const {
     welcomeScreen,
-    executeMenuOption,
-    readMenuOption
+    readMenuOption,
+    startGame,
+    wipMessage,
+    exitGame
 } = require("./src/manager")
 
-function initGame() {
-    welcomeScreen();
-    executeMenuOption(readMenuOption());
+function init() {
+    while (1) {
+        welcomeScreen();
+        let selectedMenuOption = Number(readMenuOption());
+        switch (selectedMenuOption) {
+            case 1:
+                startGame();
+                break;
+            case 2:
+                wipMessage();
+                break;
+            case 3:
+                wipMessage();
+                break;
+            case 0:
+                exitGame();
+                break;
+        }
+    }
 }
-initGame();
+init();
